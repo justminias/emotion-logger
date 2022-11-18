9c1af2438f8ad35ecada2bcb4137eb907273ed37
kmilo <kamil.kazmierczak@pm.me>
2022-11-18 22:14:17 +0100

    Fixes in saving new emotion log process

--- emotion-logger-backend/src/main/java/com/emotion/emotionlogger/converter/EmotionLogConverter.java
+++ emotion-logger-backend/src/main/java/com/emotion/emotionlogger/converter/EmotionLogConverter.java
@@ -2,16 +2,22 @@ package com.emotion.emotionlogger.converter;
 
 import com.emotion.emotionlogger.dto.EmotionLogDto;
 import com.emotion.emotionlogger.entity.EmotionLogEntity;
+import com.emotion.emotionlogger.entity.SolutionEntity;
 import com.emotion.emotionlogger.enumeration.Emotion;
+import com.emotion.emotionlogger.repository.SolutionRepository;
 import com.emotion.emotionlogger.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class EmotionLogConverter implements Converter<EmotionLogDto, EmotionLogEntity> {
 
     private final UserRepository userRepository;
+    private final SolutionRepository solutionRepository;
 
     @Override
     public EmotionLogEntity convert(EmotionLogDto emotionLogDto) {
@@ -25,6 +31,9 @@ public class EmotionLogConverter implements Converter<EmotionLogDto, EmotionLogE
                 .endTime(emotionLogDto.getEndTime())
                 .date(emotionLogDto.getDate())
                 .description(emotionLogDto.getDescription())
+                .solutions(emotionLogDto.getChosenSolutionIds().isEmpty()
+                        ? Collections.emptyList()
+                        : solutionRepository.findAllById(emotionLogDto.getChosenSolutionIds()))
                 .build();
     }
 
@@ -39,6 +48,9 @@ public class EmotionLogConverter implements Converter<EmotionLogDto, EmotionLogE
                 .endTime(emotionLogEntity.getEndTime())
                 .date(emotionLogEntity.getDate())
                 .description(emotionLogEntity.getDescription())
+                .chosenSolutionIds(emotionLogEntity.getSolutions().stream()
+                        .map(SolutionEntity::getId)
+                        .collect(Collectors.toSet()) )
                 .build();
     }
 }
