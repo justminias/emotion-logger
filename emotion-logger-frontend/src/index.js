7b0979c74cb3f195faffe367753761224fbb511e
kmilo <kamil.kazmierczak@pm.me>
2022-11-20 11:08:49 +0100

    Refactor. Creating new version of router with using 'createBrowserRouter'.

--- emotion-logger-frontend/src/index.js
+++ emotion-logger-frontend/src/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client'
-import App from './App';
-import { Provider } from "react-redux";
+import Body from './Body';
+import {Provider} from "react-redux";
 import store from './store/store';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./Home";
+import {NewEmotionLog} from "./components/emotion/new/NewEmotionLog";
+import Login from "./components/login/Login";
+import Logout from "./components/logout/Logout";
+import {SelectedDateProvider} from "./contexts/SelectedDateContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
+const router = createBrowserRouter([
+    {
+        path: "",
+        element: <Login/>
+    },
+    {
+        path: "main",
+        element: <Home/>,
+        children: [
+            {
+                path: "",
+                element: <Body/>
+            },
+            {
+                path: "log-emotion",
+                element: <NewEmotionLog/>
+            },
+            {
+                path: "edit-log/:logId",
+                element: <NewEmotionLog/>
+            }
+        ]
+    },
+    {
+        path: "login",
+        element: <Login/>
+    },
+    {
+        path: "logout",
+        element: <Logout/>
+    }
+]);
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
-        <App />
+        <SelectedDateProvider>
+            <RouterProvider router={router}/>
+        </SelectedDateProvider>
     </Provider>
-)
+);
